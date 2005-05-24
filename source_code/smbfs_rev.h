ed8def52098ffa73d7e201aed817e79f10afd586
Olaf Barthel <obarthel@gmx.net>
2005-05-24 13:22:14 +0000

    - Initial import into sourceforge.net CVS.
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/amiga-smbfs/trunk@3 26594b9e-b914-4e86-b7a1-9402bd427170

---
+++ source_code/smbfs_rev.h
@@ -0,0 +1,6 @@
+#define VERSION		1
+#define REVISION	66
+#define DATE		"10.6.2004"
+#define VERS		"smbfs 1.66"
+#define VSTRING		"smbfs 1.66 (10.6.2004)\r\n"
+#define VERSTAG		"\0$VER: smbfs 1.66 (10.6.2004)"
