9cafee23043f59516492cbf59419a74039d17570
Olaf Barthel <obarthel@gmx.net>
2009-08-31 06:34:23 +0000

    smbfs 1.74 (31.8.2009)
    - Integrated Harry Sintonen's fix for the problem caused by large
      writes to files. The respective packet size was off by one byte.
      Thank you very much!
    
    
    git-svn-id: file:///Users/olsen/Code/migration-svn-zu-git/logical-line-staging/amiga-smbfs/trunk@27 26594b9e-b914-4e86-b7a1-9402bd427170

--- source_code/smbfs_rev.h
+++ source_code/smbfs_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	73
-#define DATE		"17.4.2009"
-#define VERS		"smbfs 1.73"
-#define VSTRING		"smbfs 1.73 (17.4.2009)\r\n"
-#define VERSTAG		"\0$VER: smbfs 1.73 (17.4.2009)"
+#define REVISION	74
+#define DATE		"31.8.2009"
+#define VERS		"smbfs 1.74"
+#define VSTRING		"smbfs 1.74 (31.8.2009)\r\n"
+#define VERSTAG		"\0$VER: smbfs 1.74 (31.8.2009)"
