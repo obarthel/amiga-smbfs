7714c078925697a743c82f10140ff2ea0d087a99
Olaf Barthel <obarthel@gmx.net>
2016-04-09 11:29:22 +0200

    Version bump

--- source_code/smbfs_rev.h
+++ source_code/smbfs_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	101
-#define DATE		"4.4.2016"
-#define VERS		"smbfs 1.101"
-#define VSTRING		"smbfs 1.101 (4.4.2016)\r\n"
-#define VERSTAG		"\0$VER: smbfs 1.101 (4.4.2016)"
+#define REVISION	102
+#define DATE		"9.4.2016"
+#define VERS		"smbfs 1.102"
+#define VSTRING		"smbfs 1.102 (9.4.2016)\r\n"
+#define VERSTAG		"\0$VER: smbfs 1.102 (9.4.2016)"
