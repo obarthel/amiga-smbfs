ad1a7acd925c807c051958fe8f12e2d0bdbe5734
Olaf Barthel <o.barthel@logical-line.de>
2016-05-11 14:55:00 +0200

    Version bump

--- source_code/smbfs_rev.h
+++ source_code/smbfs_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	110
-#define DATE		"1.5.2016"
-#define VERS		"smbfs 1.110"
-#define VSTRING		"smbfs 1.110 (1.5.2016)\r\n"
-#define VERSTAG		"\0$VER: smbfs 1.110 (1.5.2016)"
+#define REVISION	111
+#define DATE		"11.5.2016"
+#define VERS		"smbfs 1.111"
+#define VSTRING		"smbfs 1.111 (11.5.2016)\r\n"
+#define VERSTAG		"\0$VER: smbfs 1.111 (11.5.2016)"
