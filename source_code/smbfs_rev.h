a044d7c145885dc457e305eb29d76b03e74c37a6
obarthel <obarthel@gmx.net>
2018-04-11 12:21:04 +0200

    Version bump

--- source_code/smbfs_rev.h
+++ source_code/smbfs_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	116
-#define DATE		"3.10.2016"
-#define VERS		"smbfs 1.116"
-#define VSTRING		"smbfs 1.116 (3.10.2016)\r\n"
-#define VERSTAG		"\0$VER: smbfs 1.116 (3.10.2016)"
+#define REVISION	117
+#define DATE		"11.4.2018"
+#define VERS		"smbfs 1.117"
+#define VSTRING		"smbfs 1.117 (11.4.2018)\r\n"
+#define VERSTAG		"\0$VER: smbfs 1.117 (11.4.2018)"
